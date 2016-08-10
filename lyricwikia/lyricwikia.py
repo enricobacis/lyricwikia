9522104bff7be07fe2c4a0d2c5920991f35fb973
enricobacis <enrico.bacis@gmail.com>
2016-08-10 17:39:49 +0200

    add urlize function

--- lyricwikia/lyricwikia.py
+++ lyricwikia/lyricwikia.py
@@ -3,12 +3,16 @@ import requests
 
 __BASE_URL__ = 'http://lyrics.wikia.com/wiki'
 
+def urlize(string):
+    """Convert string to LyricWikia format"""
+    return '_'.join(string.title().split())
+
 def create_url(artist, song):
     """Create the URL in the LyricWikia format"""
     return '{base_url}/{artist}:{song}'.format(
             base_url=__BASE_URL__,
-            artist='_'.join(artist.title().split()),
-            song='_'.join(song.title().split()))
+            artist=urlize(artist),
+            song=urlize(song))
 
 def get_lyrics(artist, song, linesep='\n'):
     """Retrieve the lyrics of the song"""
