7772d4ce287190dab4c5d8998ff90d8e0962776b
enricobacis <enrico.bacis@gmail.com>
2018-01-10 10:58:25 +0100

    export LyricsNotFound in __init__

--- lyricwikia/__init__.py
+++ lyricwikia/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['__version__', 'get_lyrics', 'Song', 'Artist']
+__all__ = ['__version__', 'get_lyrics', 'Song', 'Artist', 'LyricsNotFound']
 
 from .lyricwikia import get_lyrics, Song, Artist
+from .lyricwikia import LyricsNotFound
 from .version import __version__
